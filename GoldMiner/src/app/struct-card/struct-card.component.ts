0b60806047ed2123ae550052d7c788669449b4ee
DamianWDev <damian.wisniewski.e@gmail.com>
2022-01-14 00:50:12 +0100

    structure list and card templates

---
+++ GoldMiner/src/app/struct-card/struct-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-struct-card',
+  templateUrl: './struct-card.component.html',
+  styleUrls: ['./struct-card.component.css']
+})
+export class StructCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ GoldMiner/src/app/struct-list/struct-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-struct-list',
+  templateUrl: './struct-list.component.html',
+  styleUrls: ['./struct-list.component.css']
+})
+export class StructListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
